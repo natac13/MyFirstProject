9494dec5caa647d47dd7d7fb9f3d6e1e70e336b5
natac13 <natac13@users.noreply.github.com>
2015-01-23 12:37:51 -0500

    Adding docstring details

--- edxPS2b.py
+++ edxPS2b.py
@@ -1,3 +1,11 @@
+'''
+calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months
+fixed monthly payment of $10 amounts
+
+returns: integer which is the lowestest possible payment
+'''
+
+
 balance = float(raw_input("Balance?? "))
 annualInterestRate = float(raw_input("Annual Interest, as decimal: "))
 
